c2b22e3c89c033ad8907ab29965d1de02fec5960
Nahal Esmaeili <nesmaeili@myseneca.ca>
2017-12-10 21:19:55 -0500

    Add files via upload

---
+++ Assignment4/Assignment4/Controllers/Invoice_vm.cs
@@ -0,0 +1,98 @@
+﻿using Assignment4.Models;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Web;
+
+namespace Assignment4.Controllers
+{
+    public class InvoiceBase
+    {
+        public InvoiceBase() { }
+
+        [Key]
+        public int InvoiceId { get; set; }
+
+        public int CustomerId { get; set; }
+
+        public DateTime InvoiceDate { get; set; }
+
+        [StringLength(70)]
+        public string BillingAddress { get; set; }
+
+        [StringLength(40)]
+        public string BillingCity { get; set; }
+
+        [StringLength(40)]
+        public string BillingState { get; set; }
+
+        [StringLength(40)]
+        public string BillingCountry { get; set; }
+
+        [StringLength(10)]
+        public string BillingPostalCode { get; set; }
+
+        public decimal Total { get; set; }
+    }
+
+    public class InvoiceWithDetail : InvoiceBase
+    {
+        public InvoiceWithDetail() { }
+
+        [Required]
+        [StringLength(40)]
+        public string CustomerFirstName { get; set; }
+
+        [Required]
+        [StringLength(40)]
+        public string CustomerLastName { get; set; }
+
+        [StringLength(70)]
+        public string CustomerAddress { get; set; }
+
+        [StringLength(40)]
+        public string CustomerState { get; set; }
+
+        [Required]
+        [StringLength(40)]
+        public string CustomerEmployeeFirstName { get; set; }
+
+        [Required]
+        [StringLength(40)]
+        public string CustomerEmployeeLastName { get; set; }
+
+        public ICollection<InvoiceLineWithDetail> InvoiceLines { get; set; }
+
+    }
+
+    public class InvoiceLineBase
+    {
+        public InvoiceLineBase() { }
+
+        [Key]
+        public int InvoiceLineId { get; set; }
+
+        public int InvoiceId { get; set; }
+
+        public int TrackId { get; set; }
+
+        [Column(TypeName = "numeric")]
+        public decimal UnitPrice { get; set; }
+
+        public int Quantity { get; set; }
+    }
+
+    public class InvoiceLineWithDetail : InvoiceLineBase
+    {
+        public InvoiceLineWithDetail() { }
+
+        public InvoiceBase Invoice { get; set; }
+        public string TrackName { get; set; }
+        public string TrackAlbumTitle { get; set; }
+        public string TrackAlbumArtistName { get; set; }
+        public string TrackMediaTypeName { get; set; }
+        public string TrackComposer { get; set; }
+    }
+}
---
+++ Assignment4/Assignment4/Controllers/Manager.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+// new...
+using AutoMapper;
+using Assignment4.Models;
+
+namespace Assignment4.Controllers
+{
+    public class Manager
+    {
+        // Reference to the data context
+        private DataContext ds = new DataContext();
+
+        public Manager()
+        {
+            // Turn off the Entity Framework (EF) proxy creation features
+            // We do NOT want the EF to track changes - we'll do that ourselves
+            ds.Configuration.ProxyCreationEnabled = false;
+
+            // Also, turn off lazy loading...
+            // We want to retain control over fetching related objects
+            ds.Configuration.LazyLoadingEnabled = false;
+
+            // If necessary, add more constructor code here...
+
+        }
+
+        // Add methods below
+        // Controllers will call these methods
+        // Ensure that the methods accept and deliver ONLY view model objects and collections
+        // The collection return type is almost always IEnumerable<T>
+
+        // Suggested naming convention: Entity + task/action
+        // For example:
+        // ProductGetAll()
+        // ProductGetById()
+        // ProductAdd()
+        // ProductEdit()
+        // ProductDelete()
+
+        //STEPS FOR PROJECTS:
+        /*
+         1 - DM, DataContext
+         2 - VM (InvoiceBase, InvoiceWithCustomer : Base)
+         3 - AutoMapper
+         4 - Manger methods
+         5 - Controller (1st)
+         6 - Views
+         */
+
+        public IEnumerable<InvoiceBase> InvoiceGetall()
+        {
+            return Mapper.Map<IEnumerable<Invoice>, IEnumerable<InvoiceBase>>(ds.Invoices);
+        }
+
+        public InvoiceBase InvoiceGetById(int id)
+        {
+            var o = ds.Invoices.Find(id);
+            return (o == null) ? null : Mapper.Map<Invoice, InvoiceBase>(o);
+        }
+
+        public InvoiceWithDetail InvoiceWithDetailsGetById(int id)
+        {
+            //chain Include("XX").Include("YY")..
+
+            //var invoice = ds.Invoices.Include("Customer.Employee").Include("InvoiceLines.Track.Album.Artist").Include("InvoiceLines.Track.MediaType").SingleOrDefault(i=> i.InvoiceId == id);
+            var invoice = ds.Invoices.Include("Customer.Employee").Include("Customer").
+                Include("InvoiceLines.Track.Album.Artist").Include("InvoiceLines.Track.MediaType").SingleOrDefault(ce => ce.InvoiceId == id);
+            return (invoice == null) ? null : Mapper.Map<Invoice, InvoiceWithDetail>(invoice);
+        }
+    }
+}
