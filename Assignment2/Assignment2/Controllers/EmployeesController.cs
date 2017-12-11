1309e1b0329e691f586d40048f9f12c8bb47905e
Nahal Esmaeili <nesmaeili@myseneca.ca>
2017-12-10 21:05:26 -0500

    Add files via upload

---
+++ Assignment2/Assignment2/Controllers/EmployeesController.cs
@@ -0,0 +1,104 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Assignment2.Controllers
+{
+    public class EmployeeController : Controller
+    {
+        private Manager m = new Manager();
+        // GET: Employee
+        public ActionResult Index()
+        {
+            return View(m.EmployeeGetAll());
+        }
+
+        // GET: Employee/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id != null)
+                return View(m.EmployeeGetById(id.GetValueOrDefault()));
+            else return HttpNotFound();
+        }
+
+        // GET: Employee/Create
+        public ActionResult Create() // get empty form
+        {
+            return View(new EmployeeAdd());
+        }
+
+        // POST: Employee/Create
+        [HttpPost]
+        public ActionResult Create(EmployeeAdd e)
+        {
+            try
+            {
+                // TODO: Add insert logic here
+                if (e != null)
+                {
+                    var obj = m.EmployeeAddNew(e);
+                    if (obj != null)
+                    {
+                        return RedirectToAction("Details/" + obj.EmployeeId);
+                    }
+                }
+                else
+                {
+                    return HttpNotFound();
+                }
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: Employee/Edit/5
+        public ActionResult Edit(int id)
+        {
+            return View();
+        }
+
+        // POST: Employee/Edit/5
+        [HttpPost]
+        public ActionResult Edit(int id, FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add update logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: Employee/Delete/5
+        public ActionResult Delete(int id)
+        {
+            return View();
+        }
+
+        // POST: Employee/Delete/5
+        [HttpPost]
+        public ActionResult Delete(int id, FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add delete logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
---
+++ Assignment2/Assignment2/Controllers/Manager.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+// new...
+using AutoMapper;
+using Assignment2.Models;
+
+namespace Assignment2.Controllers
+{
+    public class Manager
+    {
+        // Reference to the data context
+        private DataContext ds = new DataContext();
+
+        public Manager()
+        {
+            // Turn off the Entity Framework (EF) proxy creation features
+            // We do NOT want the EF to track changes - we'll do that ourselves
+            ds.Configuration.ProxyCreationEnabled = false;
+
+            // Also, turn off lazy loading...
+            // We want to retain control over fetching related objects
+            ds.Configuration.LazyLoadingEnabled = false;
+
+            // If necessary, add more constructor code here...
+
+        }
+
+        // Add methods below
+        // Controllers will call these methods
+        // Ensure that the methods accept and deliver ONLY view model objects and collections
+        // The collection return type is almost always IEnumerable<T>
+
+        // Suggested naming convention: Entity + task/action
+        // For example:
+        // ProductGetAll()
+        // ProductGetById()
+        // ProductAdd()
+        // ProductEdit()
+        // ProductDelete()
+
+        public IEnumerable<EmployeeBase> EmployeeGetAll()
+        {
+            return Mapper.Map<IEnumerable<Employee>, IEnumerable<EmployeeBase>>(ds.Employees);
+        }
+
+
+        public EmployeeBase EmployeeGetById(int id)
+        {
+            var valid = ds.Employees.Find(id);
+            return (valid == null) ? null : Mapper.Map<Employee, EmployeeBase>(valid);
+        }
+
+        public EmployeeBase EmployeeAddNew(EmployeeAdd e)
+        {
+            var obj = ds.Employees.Add(Mapper.Map<EmployeeAdd, Employee>(e));
+            ds.SaveChanges();
+            return (obj == null) ? null : Mapper.Map<Employee, EmployeeBase>(obj);
+        }
+    }
+}
